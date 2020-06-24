62064905be19fe543373ac1932aea2cddebd440d
Shashank <svivek2@slb.com>
2020-06-25 00:34:35 +0530

    added cart btn

--- e-food/src/app/shared-kernel/shared-kernel.module.ts
+++ e-food/src/app/shared-kernel/shared-kernel.module.ts
@@ -5,6 +5,8 @@ import {
   MatButtonModule,
   MatMenuModule,
   MatCardModule,
+  MatIconModule,
+  MatBadgeModule,
 } from '@angular/material';
 
 @NgModule({
@@ -15,7 +17,9 @@ import {
     MatButtonModule,
     MatMenuModule,
     MatCardModule,
+    MatIconModule,
+    MatBadgeModule
   ],
-  exports: [MatToolbarModule, MatButtonModule, MatMenuModule, MatCardModule],
+  exports: [MatToolbarModule, MatButtonModule, MatMenuModule, MatCardModule, MatIconModule,MatBadgeModule],
 })
 export class SharedKernelModule {}
