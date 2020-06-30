de6f0b9ef5735c61b81e451c1ecce31fad4bdaac
Shashank <svivek2@slb.com>
2020-07-01 01:57:28 +0530

    handled cookie

--- e-food/src/app/products/products.service.ts
+++ e-food/src/app/products/products.service.ts
@@ -1,14 +1,19 @@
 import { IProduct } from './product.model';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable, NgModule } from '@angular/core';
 import { of, Observable } from 'rxjs';
-
 
 @Injectable()
 export class ProductsService {
   constructor(public http: HttpClient) {}
 
   getProductsByGroupId(grpId: number): Observable<IProduct[]> {
-    return this.http.get<IProduct[]>(`http://127.0.0.1:8080/productListBySubCategory/${grpId}`);
+    const httpOptions = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+      withCredentials: true,
+    };
+    return this.http.get<IProduct[]>(
+      `/productListBySubCategory/${grpId}`, httpOptions
+    );
   }
 }
