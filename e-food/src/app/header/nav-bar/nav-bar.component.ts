8b2592dc8523de6282e8f685bd61a52f1a9a98d1
Shashank <svivek2@slb.com>
2020-07-05 13:08:27 +0530

    combined observables into 1

--- e-food/src/app/header/nav-bar/nav-bar.component.ts
+++ e-food/src/app/header/nav-bar/nav-bar.component.ts
@@ -3,45 +3,53 @@ import { CartPreviewComponent } from './../cart-preview/cart-preview.component';
 import { HeaderService, Category, ICartItem } from './../header.service';
 import { IProduct } from './../../products/product.model';
 import { CartService } from './../../cart/cart.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { MatDialog } from '@angular/material';
+import { forkJoin, combineLatest, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss'],
 })
-export class NavBarComponent implements OnInit {
-  cartItemCount = 0;
+export class NavBarComponent implements OnInit, OnDestroy {
   cartItems: ICartItem[] = [];
   menuItems: Category[] = [];
+  SubscriptionGroup = new Subscription();
+
   constructor(
     public dialog: MatDialog,
     public cartSvc: CartService,
     public headerSvc: HeaderService,
     public sharedSvc: SharedService
   ) {}
-
-  ngOnInit() {
-    this.headerSvc.getMenuItems().subscribe((items) => {
-      this.menuItems = items;
-    });
-    // Unsubscribe
-    this.headerSvc.getCartPreview().subscribe((cartItems) => {
-      this.cartItemCount = cartItems.length;
-      this.cartItems = cartItems;
-    });
-    this.headerSvc.addGuestSessionInfo().subscribe((res) => {
-      // console.log(res);
-    }, err => {
-      this.sharedSvc.showSnackBar('Please enable cookie to work with this site');
-    });
+  ngOnDestroy(): void {
+    throw new Error('Method not implemented.');
   }
 
-  openCartModal() {
+  ngOnInit() {
+    this.SubscriptionGroup.add(combineLatest([
+      this.headerSvc.getMenuItems(),
+      this.headerSvc.getCartPreview(),
+      this.headerSvc.addGuestSessionInfo(),
+
+    ]).subscribe(responseArray => {
+      console.log(responseArray);
+      this.menuItems = responseArray[0];
+      this.cartItems = responseArray[1];
+    }, err => {
+      this.sharedSvc.showSnackBar('Error loading contents');
+    }));
+  }
+
+  openCartModal(): void {
     this.dialog.open(CartPreviewComponent, {
       width: '500px',
       height: '500px'
     });
+  }
+
+  OnDestroy() {
+    this.SubscriptionGroup.unsubscribe();
   }
 }
