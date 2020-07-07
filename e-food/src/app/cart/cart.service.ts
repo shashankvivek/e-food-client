4e8e85fcb37e341f471c8c38e091476b371e67af
Shashank <svivek2@slb.com>
2020-07-07 16:35:46 +0530

    added css for cart grp

--- e-food/src/app/cart/cart.service.ts
+++ e-food/src/app/cart/cart.service.ts
@@ -1,17 +1,94 @@
-import { ICartItem } from './../shared-kernel/shared.model';
+import {
+  ICartItem,
+  ICartSuccessResponse,
+} from './../shared-kernel/shared.model';
 import { HttpClient } from '@angular/common/http';
 import { IProduct } from './../products/product.model';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 import { take } from 'rxjs/operators';
 
 @Injectable()
 export class CartService {
+  constructor(public http: HttpClient) {}
 
-  constructor(public http: HttpClient) { }
-
-  getCheckoutCartItems(): Observable<ICartItem[]> {
-    return this.http.get<ICartItem[]>('/v1/checkoutCart');
+  getCheckoutCartItems(): Observable<any> {
+    // return this.http.get<ICartItem[]>('/v1/checkoutCart');
+    return of([
+      {
+        ruleSetId: 1,
+        currency: 'Rs',
+        items: [
+          {
+            currency: 'Rs',
+            imageUrl:
+              'https://raw.githubusercontent.com/shashankvivek/e-food-client/master/e-food/src/assets/apple-1.png',
+            productId: 1,
+            productName: 'Red Apple',
+            quantity: 3,
+            unitPrice: 10,
+            totalPrice: 30
+          },
+          {
+            currency: 'Rs',
+            imageUrl:
+              'https://raw.githubusercontent.com/shashankvivek/e-food-client/master/e-food/src/assets/apple-3.jpg',
+            productId: 2,
+            productName: 'Gree Apple',
+            quantity: 4,
+            unitPrice: 15,
+            totalPrice: 60
+          },
+        ],
+        actualPrice: 90.0,
+        discountPercent: 10,
+        discountedPrice: 80.0,
+      },
+      {
+        ruleSetId: 2,
+        currency: 'Rs',
+        items: [
+          {
+            currency: 'Rs',
+            imageUrl:
+              'https://raw.githubusercontent.com/shashankvivek/e-food-client/master/e-food/src/assets/banana.jpg',
+            productId: 3,
+            productName: 'Banana',
+            quantity: 2,
+            unitPrice: 10,
+            totalPrice: 20
+          },
+          {
+            currency: 'Rs',
+            imageUrl: 'https://raw.githubusercontent.com/shashankvivek/e-food-client/master/e-food/src/assets/pears.jpg',
+            productId: 5,
+            productName: 'Pear',
+            quantity: 4,
+            unitPrice: 30,
+            totalPrice: 120
+          },
+        ],
+        actualPrice: 140.0,
+        discountPercent: 30,
+        discountedPrice: 98.0,
+      },
+      {
+        currency: 'Rs',
+        imageUrl:
+          'https://raw.githubusercontent.com/shashankvivek/e-food-client/master/e-food/src/assets/oranges.jpg',
+        productId: 4,
+        productName: 'Oranges',
+        quantity: 5,
+        unitPrice: 20,
+        totalPrice: 100
+      },
+    ]);
   }
 
+  postProductQtyForUser(item: ICartItem): Observable<ICartSuccessResponse> {
+    return this.http.post<ICartSuccessResponse>('/v1/user/cart', {
+      totalQty: item.quantity,
+      productId: item.productId,
+    });
+  }
 }
--- e-food/src/app/cart/cart/cart.component.ts
+++ e-food/src/app/cart/cart/cart.component.ts
@@ -1,9 +1,10 @@
-import { AuthService } from 'src/app/shared-kernel/auth.service';
+import { UtilService } from 'src/app/shared-kernel/util.service';
 import { ICartItem } from './../../shared-kernel/shared.model';
+import { AuthService } from 'src/app/shared-kernel/auth.service';
 import { CartService } from './../cart.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { forkJoin, combineLatest, Subscription } from 'rxjs';
-import { take } from 'rxjs/operators';
+import { take, finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-cart',
@@ -11,23 +12,36 @@ import { take } from 'rxjs/operators';
   styleUrls: ['./cart.component.scss'],
 })
 export class CartComponent implements OnInit {
-  cartItems: ICartItem[];
+  cartItems: any[];
   userName: string;
   qtyList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-
-  constructor(public cartSvc: CartService, public auth: AuthService) {}
+  loading = false;
+  constructor(public cartSvc: CartService, public auth: AuthService, public utilSvc: UtilService) {}
 
   ngOnInit() {
+    this.prepareCart();
+  }
+
+  prepareCart() {
+    this.loading = true;
     // not using forkJoin as there is a bug in the auth0/angular2-jwt library which I used
     // to save time
     combineLatest([
       this.cartSvc.getCheckoutCartItems(),
       this.auth.getUserInfoFromToken(),
     ])
-      .pipe(take(1))
+      .pipe(take(1), finalize(() => {this.loading = false; }) )
       .subscribe((res) => {
         this.cartItems = res[0];
         this.userName = res[1].fname;
       });
   }
+
+  QtyChanged(item: ICartItem) {
+    this.cartSvc.postProductQtyForUser(item).subscribe(res => {
+      this.utilSvc.showSnackBar(res.message);
+      this.prepareCart();
+    });
+    }
+
 }
